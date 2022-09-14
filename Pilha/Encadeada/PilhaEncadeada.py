c043f99522f6105bf17f8998f5f38feaa67aa01c
alicvictor <alicvictor11@gmail.com>
2022-09-14 11:47:40 -0300

    encadeada ok

--- Pilha/Encadeada/PilhaEncadeada.py
+++ Pilha/Encadeada/PilhaEncadeada.py
@@ -4,89 +4,123 @@ class PilhaException(Exception):
 
 
 class No:
-    def __init__(self, carga: any):
-        self.carga = carga
+    def __init__(self, conteudo: any):
+        self.conteudo = conteudo
         self.prox = None
 
     def __str__(self):
-        return str(self.carga)
+        return str(self.conteudo)
 
 
 class Pilha:
+
+    '''
+            ~ RECEITA DE BOLO PARA PERCORRER UMA PILHA/LISTA ENCADEADA ~
+    
+        cursor = self.__head #Armazene o nó atual numa variável 'cursor'
+        while (CONDIÇÃO): #EX: cursor != None
+            cursor = cursor.prox #Atualize o valor do cursor para ser o próximo nó
+
+
+        ==================================================================================
+
+        deve ter mais formas de mexer com encadeamento ...
+        fazer a head apontar pro tail e o tail apontar pro head faz uma estrutura circular
+        cabecinha com bundinha & bundinha com cabecinha  :)
+
+    '''
+
     def __init__(self):
-        self.__start = None
+        self.__head = None
         self.__tamanho = 0
 
     def estaVazia(self)->bool:
-        return self.__start == None
+        return self.__head == None
 
     def tamanho(self)->int:
-        '''
-        # Percorre todos os nós de uma estrutura linear
-        cont = 0
-        cursor = self.__head
-        while(cursor != None):
-            cont += 1
-            cursor = cursor.prox
-        return cont
-        '''
         return self.__tamanho
 
     def __len__(self)->int:
         return self.__tamanho
 
     def elemento(self, posicao:int)->any:
-        '''
-        Retorna a carga armazenada no nó indicado por "posicao"
-        '''
+        #Retorna a carga armazenada no nó na posição N
         try:
             assert posicao > 0 and posicao <= self.__tamanho
-            deslocamento = self.__tamanho - posicao
-
-            cont = 0
-            cursor = self.__start
-            while( deslocamento > cont):
-                cont += 1
-                cursor = cursor.prox
-
-            return cursor.carga
+            cont = self.__tamanho
+            atual = self.__head
+            while cont != posicao:
+                atual = atual.prox
+                cont -= 1
+            return atual.conteudo
         except AssertionError:
-            raise PilhaException(f'Posicao inválida para a pilha atual com {len(self.__dados)} elementos')
+            raise PilhaException(f'Posicao inválida para a pilha atual com {self.__tamanho} elementos')
     
-    def busca(self, conteudo:any)->int:
-        cursor = self.__start
+    def busca(self, conteudo:any)->int: 
+        #Retorna a posição do nó cuja conteúdo corresponde à consulta
         cont = 0
-        while(cursor != None):
-            if cursor.carga == conteudo:
+        atual = self.__head
+        while atual != None:
+            if atual.conteudo == conteudo:
                 return self.__tamanho - cont
+            atual = atual.prox
             cont += 1
-            cursor = cursor.prox
         raise  PilhaException(f'Valor {conteudo} não está na pilha')
 
     def modificar(self, posicao:int, conteudo: any):
+        #Substitui o conteúdo do elemento na posição N
         try:
-            self.__dados[posicao-1] = conteudo
-        except IndexError:
-            raise PilhaException(f'Posicao inválida para a pilha atual com {len(self.__dados)} elementos')
+            assert posicao > 0 and posicao <= self.__tamanho
+            cont = self.__tamanho
+            atual = self.__head
+            while cont != posicao:
+                atual = atual.prox
+                cont -= 1
+            atual.conteudo = conteudo
+
+        except AssertionError:
+            raise PilhaException(f'Posicao inválida para a pilha atual com {self.__tamanho} elementos')
 
     def empilha(self, conteudo:any):
-        self.__dados.append(conteudo)
+        #Empilha um novo nó "newno"
+        newno = No(conteudo)
+        newno.prox = self.__head
+        self.__head = newno
+        self.__tamanho += 1
 
     def desempilha(self)->any:
+        #Desempilha o nó head atual
         if self.estaVazia():
             raise PilhaException(f'Pilha vazia.')
-        return self.__dados.pop()
+        head = self.__head.conteudo
+        self.__head = self.__head.prox  
+        self.__tamanho -= 1
+        return head
+        
 
     def __str__(self):
-        s = ''
-        for e in self.__dados:
-            s+=f'{e} '
+        s = '['
+        atual = self.__head
+        while atual != None:
+            s+=f'{atual}, '
+            atual = atual.prox
+        s = f'{s[:len(s)-2]}]' #Isso daqui é só pra cortar a vírgula e espaço do último loop do while sem precisar fazer nenhum if chatinho(preguiça)
         return s
 
     def esvazia(self):
-        self.__dados.clear()
+        self.__head = None
+        self.__tamanho = 0
 
+        '''
+            ~ SOLUÇÕES PARA LÍNGUAS ALÉM DE PYTHON ~
+        1.
+        while not self.estaVazia():
+            self.desempilha()
 
-
-
-
+        2.
+        try:
+            while self.desempilha():
+                pass
+        except:
+            pass
+        '''
--- Pilha/Encadeada/mainPilha.py
+++ Pilha/Encadeada/mainPilha.py
@@ -1,13 +1,29 @@
-from PilhaSequencial import *
+#Inicialização
+from PilhaEncadeada import *
 
 numeros = [4,5,6,90,56,31,42,57]
-
 pilha = Pilha()
 for n in numeros:
     pilha.empilha(n)
 
-print('Array: ',numeros)
-print('Pilha: ', pilha)
+print(f'Pilha: {pilha}')
+
+#Testando modificação
+pilha.modificar(3, 69420)
+print(f'Pilha : {pilha}')
+
+#Testando pesquisas
+print(pilha.elemento(3))
+print(pilha.busca(69420))
+
+#Testando desempilhamento (somente do topo)
+print(pilha.desempilha())
+print(f'Pilha : {pilha}')
+
+
+'''
+
+TESTES DO PROF ALEX
 
 string = 'Instituto Federal da Paraiba'
 pilha.esvazia()
@@ -17,7 +33,7 @@ for s in string:
 print()
 
 while(not pilha.estaVazia()):
-    print(pilha.desempilha(),end='')
+   print(pilha.desempilha(),end='')
 
 
 
@@ -26,4 +42,4 @@ try:
     print(pilha.busca(90))
 except PilhaException as pe:
     print(pe)
-
+'''
