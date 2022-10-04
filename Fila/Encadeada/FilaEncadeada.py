8c6fb0b80e39e622438c5cd9589c767316bf7716
alicvictor <alicvictor11@gmail.com>
2022-10-04 09:33:41 -0300

    fila encadeada

--- Fila/Encadeada/FilaEncadeada.py
+++ Fila/Encadeada/FilaEncadeada.py
@@ -2,118 +2,117 @@ class FilaException(Exception):
     def __init__(self, msg):
         super().__init__(msg)
 
-
-class NoCabeca:
-    def __init__(self):
-        self.inicio = None
-        self.final  = None
-        self.tamanho = 0
-
 class No:
-    def __init__(self, carga):
-        self.carga = carga
-        self.prox = None
+    def __init__(self, carga:any):
+        self.__carga = carga
+        self.__prox = None
+
+    @property
+    def carga(self):
+        return self.__carga
+
+    @property
+    def prox(self):
+        return self.__prox
+
+    @carga.setter
+    def carga(self, novaCarga):
+        self.__carga = novaCarga
+
+    @prox.setter
+    def prox(self, novoProx):
+        self.__prox = novoProx
 
     def __str__(self):
-        return f'{self.carga}'
+        return f'{self.__carga}'
+
+
+class Head:
+    def __init__(self):
+        self.inicio = None
+        self.fim = None
+        self.tamanho = 0
 
 class Fila:
     def __init__(self):
-        self.__head = NoCabeca()
+        self.__head = Head()
 
-    def estaVazia(self)->bool:
+    def estaVazia(self):
         return self.__head.tamanho == 0
 
-    def tamanho(self)->int:
+    def tamanho(self):
         return self.__head.tamanho
 
-    def __len__(self)->int:
+    def __len__(self):
         return self.__head.tamanho
 
-    def elemento(self, posicao:int)->any:
-        try:
-            assert posicao > 0 and posicao <= self.__head.tamanho
-
-            cursor = self.__head.inicio
-            count = 1
-            while(count < posicao):
-                cursor = cursor.prox
-                count += 1
-            return cursor.carga
-        except AssertionError:
-            raise FilaException(f'Posicao inválida para a fila atual com {self.__head.tamanho} elementos')
-
-
-    def busca(self, chave:any) -> int:  # retornar a posicao de "chave" na fila
-        cursor = self.__head.inicio
-        count = 0
-
-        while (cursor is not None):
-            count += 1
-            if cursor.carga == chave:
-                return count
-            cursor = cursor.prox
-
-        raise FilaException(f'A chave {chave} não está na Fila.')
-
-
-    def modificar(self, posicao:int, conteudo:any):
-        try:
-            assert posicao > 0 and posicao <= self.__head.tamanho
-
-            cursor = self.__head.inicio
-            cont = 1
-            while (cont < posicao):
-                cursor = cursor.prox
-                cont += 1
-            cursor.carga = conteudo
-        except AssertionError:
-            raise FilaException(f'Posicao inválida para a fila atual com {self.__head.tamanho} elementos')
-
-
-    def enfileira(self, conteudo:any):
-        novo = No(conteudo)
+    
+    def enfileira(self, carga:any):
+        novo = No(carga)
 
         if self.estaVazia():
-            self.__head.inicio = novo
-            self.__head.final = novo
-        else:
-            self.__head.final.prox = novo
-            self.__head.final = novo
-
+            self.__head.inicio = self.__head.fim = novo
+        else:    
+            self.__head.fim.prox = novo
+            self.__head.fim = novo
         self.__head.tamanho += 1
-
 
     def desenfileira(self)->any:
         if self.estaVazia():
-            raise FilaException(f'Fila vazia. Não é possível a remoção')
+            raise FilaException('Fila vazia. Não há elementos para remover.')
 
         carga = self.__head.inicio.carga
-        self.__head.inicio = self.__head.inicio.prox
+        if self.__head.tamanho == 1:
+            self.__head.inicio = self.__head.fim = None
+        else:
+            self.__head.inicio = self.__head.inicio.prox
         self.__head.tamanho -= 1
         return carga
 
-
-    def primeiro(self)->any:
+    def frente(self)->any:
         if self.estaVazia():
-            raise FilaException(f'Fila vazia. Não é possível a remoção')
+            raise FilaException('Fila vazia.')
+        return self.__head.inicio.carga
 
-        carga = self.__head.inicio.carga
-        return carga
-        
-
-    def __str__(self)->str:
-        s = '[ '
+    def busca(self, chave:any)->int:
+        # Estrutura para percorrer todos os elementos de uma estrutura linear encadeada
+        # cursor = self.__head
+        # while( cursor != None ):
+        #       ... faz alguma coisa
+        #    cursor = cursor.prox
+        cont = 1
         cursor = self.__head.inicio
-
-        while (cursor is not None):
-            s += f'{cursor.carga} '
+        while( cursor != None ):
+            if cursor.carga == chave:
+                return cont
+            cont += 1
             cursor = cursor.prox
 
-        s += ']'
+        raise FilaException(f'Chave {chave} não encontrada na Fila')        
 
+    def elemento(self, posicao:int)->any:
+        try:
+            assert posicao > 0 and posicao <= len(self)
+            cont = 1
+            cursor = self.__head.inicio
+            while( cursor != None ):
+                if cont == posicao:
+                    break
+                cont += 1
+                cursor = cursor.prox
+            
+            return cursor.carga
+        except AssertionError:
+            raise FilaException('Posicao invalida para a Fila atual')        
+
+
+    def __str__(self):
+        s = 'frente-> [ '
+        # código base para percorrer qualquer estrutura linear
+        cursor = self.__head.inicio
+        while( cursor != None ):
+            s += f'{cursor.carga}, '
+            # incremento do cursor
+            cursor = cursor.prox
+        s += ' ]'
         return s
-
-    def esvazia(self)->None:
-        while(not self.estaVazia()):
-            self.desenfileira()
