846cda9c30fe3a2c1857119ccb451267a31a279c
alicvictor <alicvictor11@gmail.com>
2022-10-10 21:46:13 -0300

    aula 7/10

--- Recursividade/demonstracao.py
+++ Recursividade/demonstracao.py
@@ -1,17 +1,6 @@
 from email.errors import MultipartConversionError
 from platform import java_ver
 
-
-def potenciaYago(base, exp) -> int:
-    return potenciaAuxiliar(base,1,exp)
-
-def potenciaAuxiliar(base, exp,limite):
-    if exp > limite:
-        return 1
-    else:
-        return base * potenciaAuxiliar(base,exp+1,limite)
-
-
 '''
 1. Há uma regra/padrão que seja geral para a maioria da entrada de dados 
    do problema?
@@ -31,19 +20,70 @@ def potenciaAuxiliar(base, exp,limite):
    dos argumentos de controle de recursividade está sendo alterado
    a cada chamada recursiva, até que atinja o caso base
 '''
-def potenciaRecursiva(base, exp, n) -> int:
+
+"""
+def potenciaYago(base, exp) -> int:
+    return potenciaAuxiliar(base,1,exp)
+
+def potenciaAuxiliar(base, exp,limite):
+    if exp > limite:
+        return 1
+    else:
+        return base * potenciaAuxiliar(base,exp+1,limite)
+"""
+
+#       POTÊNCIA
+
+def potenciaIterativa(base, exp)->int:
+    resultado = 1
+    for i in range(exp):
+        resultado *= base
+    return resultado
+
+def potenciaRecursiva(base, exp, n)->int:
     print(f'Chamada {n}: base {base}, expoente {exp}')
     if exp == 0:
         return 1
     else:
         return base * potenciaRecursiva(base,exp-1,n+1)
-        
 
-def potenciaIterativa(base, exp):
-    resultado = 1
-    for i in range(exp):
-        resultado *= base
-    return resultado
+'''
+base     exp    return
+2        3      2 * potenciaRecursiva(2, 2) => 2 * 4 = 8
+2        2      2 * potenciaRecursiva(2, 1) => 2 * 2 = 4 ^
+2        1      2 * potenciaRecursiva(2, 0) => 2 * 1 = 2 ^
+2        0                1                 =>         1 ^
+'''
+
+#       FATORIAL
+
+def fatorialIterativa(n)->int:
+    if (n == 0):
+        return 1
+    fat = 1
+    for i in range(n, 0, -1):
+        fat *= i
+    return fat
+print('Fatorial Iterativa: ', fatorialIterativa(4))
+
+def fatorialRecursiva(n)->int:
+    if (n < 0):
+        return -1
+    if (n == 0):
+        return 1
+    else:
+        return (n * fatorialRecursiva(n-1))
+print('Fatorial Recursiva: ', fatorialRecursiva(4))
+
+'''
+n        return
+4        4 * fatorialRecursiva(3) => 4 * 6 = 24
+3        3 * fatorialRecursiva(2) => 3 * 2 = 6 ^
+2        2 * fatorialRecursiva(1) => 2 * 1 = 2 ^
+1        1 * fatorialRecursiva(0) => 1 * 1 = 1 ^
+0        1 ^
+'''
+
 
 def frec(i,j)->int:
     if i == j:
@@ -53,35 +93,47 @@ def frec(i,j)->int:
 print(frec(2,6)) # o que será exibido
 
 
-def printNumbersDescending(n):
+#       ORDEM CRESCENTE E DECRESCENTE
+
+def printNumbersDescending(n):    # Decrescente
     if n == 0:
         return
     print(n,end=' ')
     printNumbersDescending(n-1)
-printNumbersDescending(10)
+printNumbersDescending(5)
 
-def printNumbersAscending(n):
+'''
+n        print                            return
+5        5                                printNumbersDescending(4)
+4        5, 4                             printNumbersDescending(3)
+3        5, 4, 3                          printNumbersDescending(2)
+2        5, 4, 3, 2                       printNumbersDescending(1)
+1        5, 4, 3, 2, 1                    printNumbersDescending(0)
+0                                         0 ^
+'''
+
+def printNumbersAscending(n):    # Crescente
     if n == 0:
         return
     printNumbersAscending(n-1)
     print(n,end=' ')
 print()
-printNumbersAscending(10)
+printNumbersAscending(5)
 
-                              
-         
-        
-          
+'''
+n        return                        print
+5        printNumbersAscending(4)      1, 2, 3, 4
+4        printNumbersAscending(3)      1, 2, 3
+3        printNumbersAscending(2)      1, 2
+2        printNumbersAscending(1)      1
+1        printNumbersAscending(0)      
+0        0 ^
+'''      
 
-
-
-     
-     
-      
-      
 
 # main
-# 2 ^ 4 = 2 x 2 x 2 x 2
-print(potenciaIterativa(2,10))
-print(potenciaRecursiva(2,3,0))
-print(potenciaYago(2,3))
+# 2 ^ 3 = 2 x 2 x 2
+print()
+print('Potencia Iterativa: ', potenciaIterativa(2,10))
+print('Potencia Recursiva: ', potenciaRecursiva(2,3,0))
+#print(potenciaYago(2,3))
