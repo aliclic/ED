c08bf1f6687d8bd131f11001d932328c733f46ac
alicvictor <alicvictor11@gmail.com>
2022-10-16 16:48:01 -0300

    8ª questao falta

---
+++ Recursividade/lista-exercicios-9/8.py
@@ -0,0 +1,3 @@
+# Faça uma função recursiva chamada menores_rec() que receba como parâmetro um list de valores
+# numéricos e um número inteiro key. A função deve retornar quantos elementos da lista possuem valor
+# inferior a key. O protótipo da função é definido por:
